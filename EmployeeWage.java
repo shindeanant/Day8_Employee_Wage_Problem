6b1e89dbbb2b7551b221f914caafc12e37e63c44
shindeanant <shindeanant1997@gmail.com>
2021-10-05 13:40:13 +0530

    ADD:Checking Empoyee attendance by using switch case statment

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,14 +1,18 @@
 package com.Day8EmployeeWage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	int wagePerHours = 20;
 	int dailyHours = 8;
 	int dailyWage;
+	int partTimeHours = 4;
+	int partTimeWage;
+	int empHour;
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
 		System.out.println(empCheck);
-
 		if (empCheck == 1) {
 			System.out.println("Employee is Present");
 		} else {
@@ -22,11 +26,40 @@ public class EmployeeWage {
 		System.out.println("Employee Daily wage = " + dailyWage);
 	}
 
+	public void employeePartTimeWage() {
+
+		partTimeWage = wagePerHours * partTimeHours;
+
+		System.out.println("Employee Part Time Wage :" + partTimeWage);
+	}
+
+	public void bySwitchCase() {
+		Random ran = new Random();
+		int empCheck = ran.nextInt(3);
+		System.out.println("Random Integers: " + empCheck);
+		switch (empCheck) {
+		case 1:
+			empHour = 8;
+			System.out.print("Present");
+			break;
+		case 2:
+			empHour = 4;
+			System.out.print("Part time");
+			break;
+		case 0:
+			empHour = 0;
+			System.out.println("Absent");
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		EmployeeWage obj = new EmployeeWage();
 		obj.employeeAttdence();
 		obj.dailyWage();
+		obj.employeePartTimeWage();
+		obj.bySwitchCase();
 
 	}
 
