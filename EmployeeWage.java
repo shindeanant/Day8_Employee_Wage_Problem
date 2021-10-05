0a3c92b9591ab8f719718a4ae724e42447647ff8
shindeanant <shindeanant1997@gmail.com>
2021-10-05 13:49:54 +0530

    ADD: Calculating Monthly wage of Employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,6 +9,8 @@ public class EmployeeWage {
 	int partTimeHours = 4;
 	int partTimeWage;
 	int empHour;
+	int monthlyWage;
+	int daysPerMonth = 20;
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
@@ -36,7 +38,7 @@ public class EmployeeWage {
 	public void bySwitchCase() {
 		Random ran = new Random();
 		int empCheck = ran.nextInt(3);
-		System.out.println("Random Integers: " + empCheck);
+		// System.out.println("Random Integers: " + empCheck);
 		switch (empCheck) {
 		case 1:
 			empHour = 8;
@@ -51,15 +53,22 @@ public class EmployeeWage {
 			System.out.println("Absent");
 			break;
 		}
+		System.out.println();
+	}
+
+	public void monthlyWage() {
+		monthlyWage = dailyWage * daysPerMonth;
+		System.out.println("Monthly Wage Of Employee is =" + monthlyWage);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		EmployeeWage obj = new EmployeeWage();
+		EmployeeWage obj = new EmployeeWage(); // Create Object
 		obj.employeeAttdence();
 		obj.dailyWage();
 		obj.employeePartTimeWage();
 		obj.bySwitchCase();
+		obj.monthlyWage();
 
 	}
 
